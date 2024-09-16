7b6a98478f0744afae2dc9ee5d1c3fd11f18c859
ooyuo <doyounjeong0@gmail.com>
2024-09-16 18:21:38 +0900

    feat: tailwindCSS setup

---
+++ app/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">it works</h1>
+    </div>
+  );
+};
+
+export default Home;
